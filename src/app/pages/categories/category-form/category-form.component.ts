5abbb45a24e9214f9a97f217621a9484b29f71df
Vitor Piovezam <vitorpiovezam@yandex.com>
2019-03-30 23:05:18 -0300

    added category form methods in component

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
+import { switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PARAMETERS } from '@angular/core/src/util/decorators';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submitingForm: false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
+  }
+
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  private setPageTitle() {
+    if (this.currentAction === 'edit') {
+      this.pageTitle = 'Editar ' + this.category.name || ' ';
+    } else {
+      this.pageTitle = 'Nova categoria';
+    }
+  }
+
+  private setCurrentAction() {
+    this.currentAction = this.route.snapshot.url[0].path;
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if (this.currentAction === 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get('id')))
+      )
+      .subscribe(
+        (category) => {
+          this.category = category;
+          this.categoryForm.patchValue(this.category);
+        },
+        (error) => alert('Erro' + error)
+      );
+    } else {
+
+    }
   }
 
 }
