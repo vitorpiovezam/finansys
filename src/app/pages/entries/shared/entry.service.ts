7188d25aee04280a7411f4d7958c6edc61e82a07
Vitor Goncalves Piovezam <vitorpiovezam@yandex.com>
2019-05-10 15:39:18 -0300

    entry service refactor

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,70 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, flatMap } from 'rxjs/operators';
+import { BaseResourceService } from 'src/app/shared/services/base-resource-service';
+import { Injectable, Injector } from '@angular/core';
 
 import { Entry } from './entry.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EntryService {
+export class EntryService extends BaseResourceService<Entry> {
 
-  private apiPath = 'api/entries';
-  constructor(private http: HttpClient) { }
-
-   getAll(): Observable<Entry[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategories)
-    );
+  constructor(protected injector: Injector) {
+    super('api/entries', injector);
   }
 
-  getById(id: number): Observable<Entry> {
-    const url = this.apiPath + '/' + id;
-
-    return this.http.get(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
-    );
-  }
-
-  create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
-    );
-  }
-
-  update(entry: Entry): Observable<Entry> {
-    const url = this.apiPath + '/' + entry.id;
-
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
-    );
-  }
-
-  delete(id: number): Observable<Entry> {
-    const url = this.apiPath + '/' + id;
-    return this.http.delete(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
-    );
-  }
-
-  private jsonDataToCategories(jsonData: any[]): Entry[] {
-    const entries: Entry[] = [];
-    jsonData.forEach(element => entries.push(element as Entry));
-    return entries;
-  }
-
-  private jsonDataToEntry(jsonData: any): Entry {
-    return jsonData as Entry;
-  }
-
-  private handleError(error: any): Observable<any> {
-    console.log('request error -> ', error);
-    return throwError(error);
-  }
 }
